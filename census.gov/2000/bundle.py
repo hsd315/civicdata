22389b4cf4c5864027751fc0838d45b9d93f5400
Eric Busboom <eric@clarinova.com>
2012-06-24 15:57:55 -0700

    no message

--- census.gov/2000/bundle.py
+++ census.gov/2000/bundle.py
@@ -12,8 +12,6 @@ class Bundle(Base):
     Bundle code for US 2000 Census, Summary File 1
     '''
 
-    URLS_FILE = 'urls.yaml'
-
     def __init__(self,directory=None):
         '''Constructor'''
         self.super_ = super(Bundle, self)
@@ -21,87 +19,98 @@ class Bundle(Base):
       
     def schemaGenerator(self):
         '''Return schema rows'''
-        
-        from databundles.config.orm import Table, Column
+    
+        from databundles.orm import Table, Column
         import csv
         
-        header_file = self.path(self.config['build']['headers'])
-        
+        header_file = self.filesystem.path(self.config.group('build').get('headers'))
         reader  = csv.DictReader(open(header_file, 'rbU') )
       
+        i = 0
         for row in reader:
+            if not row['TABLE']:
+                continue
+      
+            
             # The first two rows for the table give information about the title
             # and population universe. 
             if( not row['FIELDNUM']):
                 if  row['TABNO']:
                     # First of the two info rows. 
-                    table = Table(name=row['TABNO'].strip('.'),description=row['TEXT'])
-                  
-                    
-                elif row['TEXT']:
+                    last_table = row['TABLE']
+                    yield Table(name=row['TABLE'].strip(),description=row['TEXT'])
+              
+                else:
+                    last_table = row['TABLE']
                     #Second and last of the two info rows, so now is time to create the table. 
-                    table.universe = row['TEXT']
-                  
+                    yield Table(name=row['TABLE'].strip(),
+                                 universe=row['TEXT'].replace('Universe:','').strip()
+                                 )               
             else:
-                table.add_column(Column(name=row['FIELDNUM'], description=row['TEXT'],
-                                         datatype=Column.DATATYPE_INTEGER))
-        return ds
-    def process_headers(self):
-        '''Process the sf1header.csv file and return a config.Database object that holds
-        the schema'''
-        
-        from databundles.config.database import Database, Table, Column
-        import csv
-        
-        header_file = self.path(self.config['build']['headers'])
-        
-        reader  = csv.DictReader(open(header_file, 'rbU') )
+                
+                # The whole table will exist in one segment ( file number ) 
+                # but the segment id is not included on the same lines ast the
+                # table name. 
+                if last_table:
+                     yield Table(name=last_table.strip(),data={'segment':row['SEG']})
+                     last_table = False
+                         
+                yield Column(name=row['FIELDNUM'],
+                             table_name=row['TABLE'],
+                             description=row['TEXT'].strip(),
+                             datatype=Column.DATATYPE_INTEGER,
+                             data={'segment':int(row['SEG'])}
+                             )
  
-        ds = Database()
-
-        for row in reader:
-            # The first two rows for the table give information about the title
-            # and population universe. 
-            if( not row['FIELDNUM']):
-                if  row['TABNO']:
-                    # First of the two info rows. 
-                    table = Table(name=row['TABNO'].strip('.'),description=row['TEXT'])
-                        
-                    ds.add_table(table)         
-                    
-                elif row['TEXT']:
-                    #Second and last of the two info rows, so now is time to create the table. 
-                    table.universe = row['TEXT']
-                  
-            else:
-                table.add_column(Column(name=row['FIELDNUM'], description=row['TEXT'],
-                                         datatype=Column.DATATYPE_INTEGER))
-        return ds
+    def table_seg_map(self):
         
-    def get_manifest(self):
+        from databundles.orm import Table
+        
+        # Create a lookup table mapping segments to tables. 
+        #  There are multiple tables per file. This query is
+        # a horrible, encapsulation busting hack, and I am ashamed of it. 
+        s = self.database.session
+        table_seg = {}
+        for table in s.query(Table).all() :
+            if not table.data.get('segment',None):
+                raise ValueError("Should have gotten a segment data for table")
+            
+            seg = int(table.data['segment'])
+            
+            if not  seg in table_seg:
+                table_seg[seg] = []
+                
+            table_seg[seg].append(table.name.encode('ascii'))
+            
+       
+        return table_seg
+   
+    def partitionGenerator(self):
         '''Get all of the URLs of the zip files to download '''
         import urllib
         import urlparse
         import yaml
+        import re
         from bs4 import BeautifulSoup
+        from databundles.partition import Partitions
+    
    
-        urls_file = self.path(self.URLS_FILE)
-        
-        # If the file has already been generated, return it. 
-        if os.path.exists(urls_file):
-            return  yaml.load(file(urls_file, 'r'))
+        downloadDir = 'downloads'
    
-        urls = []
         i = 0;
    
+        states_file = self.filesystem.path(self.config.group('build').get('statesFile'))
+   
         # Load in a list of states, so we know which links to follow
-        with open(self.config['build']['statesFile']) as f:
+        with open(states_file) as f:
             states = map(lambda s: s.strip(),f.readlines())
         
         # Root URL for downloading files. 
-        url = self.config['build']['rootUrl']
+        url = self.config.group('build').get('rootUrl')
        
         doc = urllib.urlretrieve(url)
+        
+        self.log('Getting URLS from '+url)
         # Get all of the links
         for link in BeautifulSoup(open(doc[0])).find_all('a'):
             
@@ -116,77 +125,84 @@ class Bundle(Base):
                 # Get all of the zip files in the directory
                 for link in  BeautifulSoup(open(stateIndex[0])).find_all('a'):
                     if link.get('href') and  '.zip' in link.get('href'):
-                        urls.append({'state': state.encode('ascii', 'ignore'),
-                                     'url': urlparse.urljoin(stateUrl, link.get('href')).encode('ascii', 'ignore')
-                                     })   
-        import yaml
-            
-        with open(urls_file,'w') as f:
-            f.write(yaml.dump(urls))
-            
-        return urls
-         
-        
+                        final_url = urlparse.urljoin(stateUrl, link.get('href')).encode('ascii', 'ignore')
+
+                        path = urlparse.urlparse(final_url).path
+                      
+                        data={'source_url':final_url,
+                              'zip_file':os.path.join(downloadDir+'/'+os.path.basename(path))}
+                        
+                        if 'geo_uf1' in final_url:
+                            state = re.match('.*/(\w{2})geo_uf1', final_url).group(1)
+                          
+                            yield self.partitions.new_parition(
+                                        space=state,table='SF1GEO',data=data)
+                            
+                        else:
+                            m = re.match('.*/(\w{2})(\d{5})_uf1', final_url)
+                            state,segment = m.groups()
+                            segment = int(segment.lstrip('0'))
+
+                            # Create a table partition for every table in the segment. 
+                            for table in self.table_seg_map().get(segment):
+                             
+                                yield self.partitions.new_parition(
+                                        space=state,table=table,data=data)
+ 
+     
     def pre_prepare(self):
         return True
     
     def prepare(self):
         '''Create the prototype database'''
+       
 
-        if not self.protodb.exists():
-            print "Creating prototype database"
-            
-            ds = self.process_headers()
-            
-            proto = self.protodb
-            
-            proto.delete()
-            proto.create()
-            proto.load_sql(self.path('meta/tables.sql'))
-            #proto.add_schema(ds)
-            
-        # Create the manifast file if it does not exist. 
-        urls = self.get_manifest()  
-      
-    
-    def download(self):
-        import download
-        import urllib
-        import urlparse
-        import zipfile
-     
-        urls = self.get_manifest()  
-     
-        extractDir = self.directory('extracts')
-        downloadDir = self.directory('downloads')
-     
-        for e in urls:
-            state = e['state']
-            aurl = e['url']
-                    
-            path = urlparse.urlparse(aurl).path
-            webFilename = os.path.normpath(downloadDir+'/'+os.path.basename(path))
+        header_file = self.config.group('build').get('headers')
+       
+        self.config.add_file(header_file)
+
+        self.log(str(len(self.schema.tables))+' tables in database')
+        self.log(str(len(self.schema.columns))+' columns in database')
         
-            if(not os.path.exists(webFilename)):
-                print 'Downloading '+aurl+' to '+webFilename
-                webFilename, headers = urllib.urlretrieve(aurl, webFilename )
-            else:
-                print 'Using cached web file '+webFilename
-          
-            try :
-                with zipfile.ZipFile(webFilename) as zf:
-                    for name in  zf.namelist():
-                        extractFilename = os.path.join(extractDir,name)
-                        if(not os.path.exists(extractFilename)):
-                            print 'Extracting'+extractFilename+' from '+webFilename
-                            name = name.replace('/','').replace('..','')
-                            zf.extract(name,extractDir )
-                        else:
-                            print 'Using cached extract file '+extractFilename
-            except zipfile.BadZipfile:
-                os.unlink(webFilename)
-                print "ERROR: Not a zipfile: "+webFilename
+        if (len(self.schema.tables) != 286 or 
+            len(self.schema.columns) != 8113 or
+            self.filesystem.ref(header_file).changed):
+            self.log("Regenerating schema")
+            self.schema.generate()
+        else:
+           self.log("Re-using schema")
+      
+        
+        # Create the manifast file if it does not exist. 
+        ##urls = self.get_manifest()  
+        
+        self.partitions.generate()
+        
+        return true
+
+    from contextlib import contextmanager
     
+    @contextmanager
+    def extract_zip(self,path):
+        '''Extract a the files from a zip archive'''
+        
+        extractDir = self.filesystem.directory('extracts')
+
+        with zipfile.ZipFile(path) as zf:
+            for name in  zf.namelist():
+                extractFilename = os.path.join(extractDir,name)
+                
+                if(os.path.exists(extractFilename)):
+                    s.unlink(extractFilename)
+                    
+                print 'Extracting'+extractFilename+' from '+path
+                name = name.replace('/','').replace('..','')
+                zf.extract(name,extractDir )
+                    
+                yield extractFilename
+                os.unlink(extractFilename)
+                        
+
     def pre_transform(self):
         return True
     
@@ -290,3 +306,14 @@ class Bundle(Base):
     
     def submit(self):
         self.super_.submit()
+        
+        
+
+import sys
+
+if __name__ == '__main__':
+    import databundles.run
+      
+    databundles.run.run(sys.argv[1:], Bundle)
+    
+    
