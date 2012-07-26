9b2c4a600eb3dd3f473abd215bee8934557c9bbf
Eric Busboom <eric@clarinova.com>
2012-07-26 06:07:19 -0700

    Refactoring to make code simpler, bug fixes

--- census.gov/census.gov-2000_population-sf1geo-orig/bundle.py
+++ census.gov/census.gov-2000_population-sf1geo-orig/bundle.py
@@ -11,35 +11,70 @@ class Bundle(UsCensusBundle):
         self.super_ = super(Bundle, self)
         self.super_.__init__(directory)
         
+        bg = self.config.build
+        self.geoheaders_file = self.filesystem.path(bg.geoheaderFile)
+        
     def prepare(self):
         '''Create the prototype database'''
-        from databundles.partition import PartitionIdentity 
-        import yaml
-        
-        self.database.create()
+  
+        if not self.database.exists():
+            self.database.create()
 
         self.scrape_files()
-        self.generate_geo_schema()
-        
-        # Generate all of the partitions
-        urls = yaml.load(file(self.urls_file, 'r')) 
-        for state, source in urls['geos'].items(): #@UnusedVariable
-            self.log("Create partition for "+state)
-            # Create the partition
-            partition = self.partitions.new_partition(
-                           PartitionIdentity(self.identity, space=state))
 
-            partition.database.delete()
-            partition.database.create()
-            partition.database.load_sql( self.filesystem.path('meta/sf1geo.sql'))
-            
-        self.database.commit() 
+        if not self.schema.table('sf1geo'):
+            self.schema.schema_from_file(open(self.geoheaders_file, 'rbU'))
+
+        self.database.commit()
+        
+        self.create_partitions()
+        
         # First install the bundle main database into the library
         # so all of the tables will be there for installing the
         # partitions. 
-        self.library.put(self)
+        if self.library.get(self.identity.id_):
+            self.log("Found in bundle library, skipping. ")
+        else:
+            self.library.put(self)
         
         return True
+ 
+    def create_partitions(self):
+        from databundles.partition import PartitionIdentity 
+        import yaml
+ 
+        print "Partitions", len(self.partitions.all)
+ 
+        # Generate the state partitions
+        urls = yaml.load(file(self.urls_file, 'r')) 
+        for state, source in urls['geos'].items(): #@UnusedVariable
+
+            pid = PartitionIdentity(self.identity, space=state)
+            if not self.partitions.find(pid):
+                self.log("Create partition for "+state)
+                partition = self.partitions.new_partition(pid)
+               
+            else:
+                self.log("Already created partition, skipping "+state)
+                
+        # Generate partitions for the individual tables. 
+        for table in self.schema.tables:
+            
+            # These tables have other sources, or will get processed later. 
+            if table.name in ['sf1geo']:
+                continue
+      
+            pid = PartitionIdentity(self.identity, table=table.name)
+            if not self.partitions.find(pid):
+                self.log("Create partition for "+table.name)
+                p = self.partitions.new_partition(pid)
+                #p.create_with_tables(table.name)
+            else:
+                self.log("Already created partition, skipping "+table.name)
+                
+            
+            
+        self.database.commit() 
  
     def build(self):
         '''Create data  partitions. 
@@ -58,9 +93,19 @@ class Bundle(UsCensusBundle):
     
     def load_geo(self, state, source):
         from databundles.partition import PartitionIdentity 
-       
         import petl.fluent as petl
-        header, regex = self.get_geo_regex()
+        
+        header, regex, regex_str = self.schema.table('sf1geo').get_fixed_regex() #@UnusedVariable
+
+        pid = PartitionIdentity(self.identity, space=state)
+        partition = self.partitions.find(pid)
+       
+        if self.library.get(partition.identity.id_):
+            self.log("Found in library, skipping. "+str(pid))
+            return
+   
+        if not partition.database.exists():
+            partition.create_with_tables('sf1geo')
 
         retry = 4
         while retry > 0:
@@ -69,10 +114,6 @@ class Bundle(UsCensusBundle):
             with self.filesystem.download(source) as zip_file:
                 with self.filesystem.unzip(zip_file) as rf:
                     self.log("Processing GEO file: "+rf)
- 
-                    partition = self.partitions.find(PartitionIdentity(self.identity, space=state))
- 
-                    # Load the data
                               
                     db_path = partition.database.path
                     self.log("  Partion: "+str(partition))
@@ -81,7 +122,7 @@ class Bundle(UsCensusBundle):
                     t = petl.fromregex(rf, regex=regex, header=header) #@UndefinedVariable
  
                     if state == 'pr': # Some unicode characters, but only in this file
-                        t = t.convert('NAME', unicode)
+                        t = t.convert('name', unicode)
  
                     t.progress(100000).appendsqlite3(db_path,'sf1geo')
                           
@@ -90,6 +131,102 @@ class Bundle(UsCensusBundle):
                     partition.database.delete()
                                   
                     return True
+                
+    def combine_geo(self):
+        """Combine all of the seperate geo files into a single database, and
+        trim all of the values """
+
+        from databundles.partition import PartitionIdentity
+
+        #
+        # Get the original geo files from the library
+        #
+        l =  databundles.library.get_library()
+        q = (l.query()
+                 .identity(creator='clarinova.com', dataset='2000 Population',
+                           subset = 'sf1geo', variation='orig')
+                 .partition(any=True) # Get partitions, not just root bundle. 
+            )
+
+        combined = self.partitions.find(PartitionIdentity(self.identity, time='2000'))
+     
+        combined.database.create()
+        combined.database.create_table('sf1geo2000')
+
+        print "COMBINED", combined.database.path
+  
+        # Copy from the indidual partitions into the combined geo file, 
+        # by attaching and copying within sqlite. 
+        cdb = combined.database
+
+        for result in q.all:   
+ 
+            geo = l.get(result.Partition)
+            print "GEO",geo.database.path 
+
+            name = cdb.attach(geo.database)
+            
+            sf1t = geo.schema.table('sf1geo')
+           
+            map_ = { "TRIM({})".format(c.name): c.name for  c in sf1t.columns}
+            
+            cdb.copy_from_attached(('sf1geo','sf1geo2000'), 
+                                   name=name,
+                                   columns=map_,
+                                    on_conflict = 'IGNORE')
+            cdb.detach(name)
+ 
+
+    def split_geo_sqlite(self):
+        '''Split the geo file using attachment and table copy'''  
+    
+                
+        from databundles.partition import PartitionIdentity
+
+        #
+        # Get the original geo files from the library
+        #
+        l =  databundles.library.get_library()
+        q = (l.query()
+                 .identity(creator='clarinova.com', dataset='2000 Population',
+                           subset = 'sf1geo', variation='orig')
+                 .partition(any=True) # Get partitions, not just root bundle. 
+            )
+
+        for result in q.all:   
+
+            geo = l.get(result.Partition)
+            print "GEO",geo.database.path 
+
+            geo_col_names = [ c.name for c in geo.schema.table('sf1geo').columns ]
+
+            
+            for table in self.schema.tables:
+           
+                p = self.partitions.find(
+                        PartitionIdentity(self.identity, table=table.id_))
+                
+                pdb = p.database
+                
+                if not pdb.exists():
+                    pdb.create();
+  
+                print "PARTITION", p.name, p.identity.name, p.database.path
+                
+                pdb.create_table(table.name)
+                attach_name = pdb.attach(geo.database)
+                map_ = {}
+                for c in table.columns:
+                    if c.name in geo_col_names:
+                        map_["TRIM({})".format(c.name)] = c.name
+                
+                if map_.items():
+                    pdb.copy_from_attached(('sf1geo', table.name),
+                                            columns=map_, name=attach_name,
+                                            on_conflict = 'IGNORE')
+                
+                pdb.detach(attach_name)
+
     
 import sys
 
@@ -98,5 +235,3 @@ if __name__ == '__main__':
       
     databundles.run.run(sys.argv[1:], Bundle)
      
-    
-    
--- census.gov/census.gov-geography-dim-orig/bundle.py
+++ census.gov/census.gov-geography-dim-orig/bundle.py
@@ -6,8 +6,6 @@ from databundles.bundle import BuildBundle
 
 import databundles.library
 
- 
-
 class Bundle(BuildBundle):
     ''' '''
  
@@ -20,9 +18,7 @@ class Bundle(BuildBundle):
     def generate_schema(self):
         '''Return schema rows from the geoschema.csv file, 
         fetched from Google Docs '''
-        from databundles.orm import Column
-        import csv, re
-    
+       
         if len(self.schema.tables) > 0 and len(self.schema.columns) > 0:
             self.log("Reusing schema")
             return True
@@ -34,75 +30,7 @@ class Bundle(BuildBundle):
         import urllib2
         headers = urllib2.urlopen(self.config.build.geoheaders);
         
-        reader  = csv.DictReader(headers)
-    
-        self.log("Generating main table schemas")
-       
-        t = None
-
-        tm = {
-              'TEXT':Column.DATATYPE_TEXT,
-              'INTEGER':Column.DATATYPE_INTEGER,
-              'REAL':Column.DATATYPE_REAL,
-              }
-
-        new_table = True
-        for row in reader:
-          
-            # If the spreadsheet gets downloaded rom Google Spreadsheets, it is
-            # in UTF-8
-            row = { k:v.decode('utf8').encode('ascii','ignore').strip() for k,v in row.items()}
-
-            if not row['table']:
-                new_table = True
-                continue
-
-            if new_table and row['table']:
-                #print 'Table',row['table']
-                t = self.schema.add_table(row['table'], **row)
-                new_table = False
-              
-            # Ensure that the default doesnt get quotes if it is a number. 
-            if row['default']:
-                try:
-                    default = int(row['default'])
-                except:
-                    default = row['default']
-          
-            # Build the index and unique constraint values. 
-            indexes = [ row['table']+'_'+c for c in row.keys() if (re.match('i\d+', c) and row[c].strip())]  
-            uindexes = [ row['table']+'_'+c for c in row.keys() if (re.match('ui\d+', c) and row[c].strip())]  
-            uniques = [ row['table']+'_'+c for c in row.keys() if (re.match('u\d+', c) and  row[c].strip())]  
-    
-            datatype = tm[row['type'].strip()]
-    
-            if row['size'].strip():
-                try:
-                    size = int(row['size'])
-                except:
-                    size = None
-            else:
-                size = None
-    
-            if  size and size > 0:
-                illegal_value = '9' * size
-            else:
-                illegal_value = None
-   
-            #print 'Column',row['table'], row['column']
-            self.schema.add_column(t,row['column'],
-                                   is_primary_key=row['is_pk'],
-                                   description=row['description'].strip(),
-                                   datatype=datatype,
-                                   unique_constraints = ','.join(uniques),
-                                   indexes = ','.join(indexes),
-                                   uindexes = ','.join(uindexes),
-                                   default = default,
-                                   illegal_value = illegal_value,
-                                   size = size
-                                   )
-
-        self.database.commit()
+        self.schema.schema_from_file(headers)
   
     def generate_partitions(self):
         from databundles.partition import PartitionIdentity
@@ -133,59 +61,14 @@ class Bundle(BuildBundle):
         
         return True
     
-    def combine_geo(self):
-        """Combine all of the serverate geo files into a single database, and
-        trim all of the values """
-
-        from databundles.partition import PartitionIdentity
-
-        #
-        # Get the original geo files from the library
-        #
-        l =  databundles.library.get_library()
-        q = (l.query()
-                 .identity(creator='clarinova.com', dataset='2000 Population',
-                           subset = 'sf1geo', variation='orig')
-                 .partition(any=True) # Get partitions, not just root bundle. 
-            )
-
-        combined = self.partitions.find(PartitionIdentity(self.identity, time='2000'))
-     
-        combined.database.create()
-        combined.database.create_table('sf1geo2000')
-
-        print "COMBINED", combined.database.path
   
-        # Copy from the indidual partitions into the combined geo file, 
-        # by attaching and copying within sqlite. 
-        cdb = combined.database
-
-        for result in q.all:   
- 
-            geo = l.get(result.Partition)
-            print "GEO",geo.database.path 
-
-            name = cdb.attach(geo.database)
-            
-            sf1t = geo.schema.table('sf1geo')
-           
-            map_ = { "TRIM({})".format(c.name): c.name for  c in sf1t.columns}
-            
-            cdb.copy_from_attached(('sf1geo','sf1geo2000'), 
-                                   name=name,
-                                   columns=map_,
-                                    on_conflict = 'IGNORE')
-            cdb.detach(name)
- 
     def split_geo(self):
         '''Split the geo file into seperate tables'''
         from databundles.partition import PartitionIdentity
         from databundles.database import  insert_or_ignore
         import sqlite3
         import time
-        
 
-    
         def set_default(column, v):
             # The 'illegal' value is usually a string of '9'
             if str(v) == column.illegal_value or  ( v == '' or v is None):
@@ -223,17 +106,15 @@ class Bundle(BuildBundle):
             
             p = self.partitions.find(
                         PartitionIdentity(self.identity, table=table.id_))
-               
-            ti[table.name]['partition'] = p
+
        
             # Create database if they don't exist. We are only creating
             # the one table mentioned in the partition. 
             if not  p.database.exists():
                 p.database.create();
-                p.database.copy_table_from(self.database, table.name)
+                p.database.copy_table_from(self.database, table.id_)
                 p.schema.create_tables()
-        
-            
+         
             for column in table.columns:
                 if column.name in source_cols:
                     ti[table.name]['columns'].append(column)
@@ -247,7 +128,8 @@ class Bundle(BuildBundle):
                         f = lambda v, f=f: coerce_int(f(v))
  
                     # Set the default value. 
-                    f = lambda v, column=column, f=f : set_default(column,f(v) )
+                    if column.default.strip():
+                        f = lambda v, column=column, f=f : set_default(column,f(v) )
              
                     # Extract the value from a position in the row
                     f = lambda row, column=column, f=f: f(row[column.name])
@@ -266,8 +148,7 @@ class Bundle(BuildBundle):
             ti[table.name]['ins'] = insert_or_ignore(table.name, ti[table.name]['columns'])
             ti[table.name]['meta'] =  p.database.table(table.name)
             ti[table.name]['db'] = p.database
-        
-           
+
 
         # Now get all of the state partitions fro the library. 
         l = databundles.library.get_library()
@@ -279,8 +160,6 @@ class Bundle(BuildBundle):
 
         
         geo_i = 0;
- 
-        print ti.keys()
  
         for result in q.all:
  
@@ -316,64 +195,17 @@ class Bundle(BuildBundle):
         for table in ti.keys():
             db.dbapi_close()
 
+    ########################
+    # Old Code
  
-    def split_geo_sqlite(self):
-        '''Split the geo file using attachment and table copy'''  
-    
-                
-        from databundles.partition import PartitionIdentity
-
-        #
-        # Get the original geo files from the library
-        #
-        l =  databundles.library.get_library()
-        q = (l.query()
-                 .identity(creator='clarinova.com', dataset='2000 Population',
-                           subset = 'sf1geo', variation='orig')
-                 .partition(any=True) # Get partitions, not just root bundle. 
-            )
-
- 
-        for result in q.all:   
-
-            geo = l.get(result.Partition)
-            print "GEO",geo.database.path 
-
-            geo_col_names = [ c.name for c in geo.schema.table('sf1geo').columns ]
-
-            
-            for table in self.schema.tables:
-           
-                p = self.partitions.find(
-                        PartitionIdentity(self.identity, table=table.id_))
-                
-                pdb = p.database
-                
-                if not pdb.exists():
-                    pdb.create();
-                    
-                
-                print "PARTITION", p.name, p.identity.name, p.database.path
-                
-                pdb.create_table(table.name)
-                attach_name = pdb.attach(geo.database)
-                map_ = {}
-                for c in table.columns:
-                    if c.name in geo_col_names:
-                        map_["TRIM({})".format(c.name)] = c.name
-                
-                if map_.items():
-                    pdb.copy_from_attached(('sf1geo', table.name),
-                                            columns=map_, name=attach_name,
-                                            on_conflict = 'IGNORE')
-                
-                pdb.detach(attach_name)
-
+   
     def build(self):
     
         #self.combine_geo()
         
-        self.split_geo()
+        #self.split_geo()
+        
+        self.split_geo_sqlite()
         
         return True
 
