819df0bade48de657782f75d219378320e703e0c
Eric Busboom <eric@clarinova.com>
2013-02-05 22:17:28 -0800

    no message

--- census.gov/census.gov-geography-dim/bundle.py
+++ census.gov/census.gov-geography-dim/bundle.py
@@ -5,6 +5,7 @@
 from  databundles.bundle import BuildBundle
 import os.path
 import yaml
+from databundles.library import get_library
  
 class Bundle(BuildBundle):
     '''
@@ -19,8 +20,6 @@ class Bundle(BuildBundle):
         self.geoschema_file = self.filesystem.path(bg.geoschemaFile)
         self.states_file =  self.filesystem.path(bg.statesFile)
 
-  
-        
     def prepare(self):
         '''Scrape the URLS into the urls.yaml file and load all of the geo data
         into partitions, without transformation'''
@@ -29,38 +28,81 @@ class Bundle(BuildBundle):
         if not self.database.exists():
             self.database.create()
      
+        self.make_generated_geo()
+     
+        geo_file = self.filesystem.path(self.config.build.gengeoFile)
+     
         if len(self.schema.tables) == 0 and len(self.schema.columns) == 0:
             self.log("Loading schema from file")
-            with open(self.geoschema_file, 'rbU') as f:
+            with open(geo_file, 'rbU') as f:
                self.schema.schema_from_file(f)           
         else:
             self.log("Reusing schema")
 
+        self.schema.create_tables()
 
         self.database.session.commit()
         
         return True
 
-    def build(self):
-
-        print self.identity.name
-
-        return True       
-
-    def install(self):  
+    def make_generated_geo(self):
+        """Create a schema file for the queries to extract data from the 
+        source partition. 
+        
+        This method executed the queries defined in the 'meta' configuration, 
+        then creates a schema for that query, based on the first row. 
+        """
+        
+        ds = get_library().get('a1O5Tm') 
      
-        self.log("Install bundle")  
-        dest = self.library.put(self)
-        self.log("Installed to {} ".format(dest[2]))
-        
-        for partition in self.partitions:
-        
-            self.log("Install partition {}".format(partition.name))  
-            dest = self.library.put(partition)
-            self.log("Installed to {} ".format(dest[2]))
+        qt = self.config.meta.query2000.template
 
-        return True
-        
+        geo_file = self.filesystem.path(self.config.build.gengeoFile)
+
+        import csv
+        with open(geo_file, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(['table','column','is_pk','type'])
+            
+            for qd in self.config.meta.query2000.data:
+                
+                writer.writerow([qd[1],qd[1]+'_id', 1, 'INTEGER'])
+                writer.writerow([qd[1],'geoid', None, 'TEXT'])
+                                
+                partition = ds.bundle.partitions.find(grain=qd[0])
+                row =  partition.database.session.execute(qt.format(*qd)).first()
+                
+                for k,v in zip(row.keys(), row):
+                    
+                    try:
+                        int(v)
+                        type = "INTEGER"
+                    except:
+                        type = "TEXT"
+                        
+                    if k in ('name'):
+                        type = 'INTEGER'
+                        
+                    writer.writerow([str(qd[1]), str(k),None,type])
+                        
+    def build(self):
+           
+        ds = get_library().get('a1O5Tm') 
+        qt = self.config.meta.query2000.template
+          
+        for qd in self.config.meta.query2000.data:
+            
+            partition = ds.bundle.partitions.find(grain=qd[0])
+            print partition.identity.name, partition.database.path
+            
+            with self.database.inserter(qd[1]) as ins:
+                for row in  partition.database.session.execute(qt.format(*qd)):
+                    geo_id = 'foo'
+                    ins.insert((None, geo_id)+tuple(row))
+    
+     
+
+    
 import sys
 
 if __name__ == '__main__':
