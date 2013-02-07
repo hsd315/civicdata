61ba56f9f610ffdcf40aa014999d0108c0388a95
Eric Busboom <eric@clarinova.com>
2013-02-07 13:32:29 -0800

    Census geography

--- census.gov/census.gov-geography-dim/bundle.py
+++ census.gov/census.gov-geography-dim/bundle.py
@@ -26,34 +26,6 @@ class Bundle(BuildBundle):
         from databundles.partition import PartitionIdentity
         import re, csv, collections
       
-        geo_file = '/Users/eric/proj/github.com/civicdata/census.gov/census.gov-2000_population-sf1-geo/meta/geoschema.csv'
-        d = collections.OrderedDict()
-        with open(geo_file) as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                if  row['description']:
-                    print row['column'], row['description']
-                    d[row['column']] = row['description']
-                    
-        geo_file = '/Users/eric/proj/github.com/civicdata/census.gov/census.gov-2010_population-geo-orig/meta/geoschema.csv'
-
-        with open(geo_file) as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                if  row['description']:
-                    print row['column'], row['description']
-                    d[row['column']] = row['description']
-            
-        geo_file = self.filesystem.path(self.config.build.geoschemaFile)
-        with open(geo_file) as f:
-            reader = csv.reader(f)
-            reader.next
-            for row in reader:
-                print row
-                row[4] = d[row[1]]
-                print row
-      
-        return;
       
         if not self.database.exists():
             self.database.create()
@@ -72,7 +44,115 @@ class Bundle(BuildBundle):
         self.database.session.commit()
         
         return True
+    
+    def build(self):
 
+        source1 = get_library().get(self.config.queries.source1) 
+        source2 = get_library().get(self.config.queries.source2)  
+        template = self.config.queries.template
+        data = self.config.queries.data
+        
+        
+        for sumlev, table_name, name_remove in data:
+            
+            table = self.schema.table(table_name)
+
+            sp1 = source1.bundle.partitions.find(grain=sumlev)
+            sp2 = source2.bundle.partitions.find(grain=sumlev)
+
+            s1 = self.database.attach(sp1, 'source1')
+            s2 = self.database.attach(sp2, 'source2')
+
+            s1_fields = [c.name for c in table.columns if c.data.get('in2000', False) 
+                         and c.name not  in ('name', 'geoid') and not c.name.endswith('_id')]
+            s2_fields = [c.name for  c in table.columns if c.name not in s1_fields 
+                         and c.name not  in ('name', 'geoid') and not c.name.endswith('_id')]
+
+            fields = s1_fields + s2_fields
+            s1_fields = ['s1.'+c for c in s1_fields]
+            s2_fields = ['s2.'+c for c in s2_fields]
+            
+            joins = [ "s1.{0} = s2.{0}".format(c.name)  
+                       for  c in table.columns if c.data.get('join',False)]
+
+            select1 = [ c.name if c.data.get('in2000',False) else 'Null as {}'.format(c.name)
+                        for c in table.columns if c.name not  in ('name', 'geoid') and not c.name.endswith('_id')]
+            select1.append("trim(replace(name,'{name_remove}','')) as name1".format(name_remove=name_remove));
+            
+            select2 = [ c.name if c.data.get('in2010',False) else 'Null as {}'.format(c.name)
+                        for c in table.columns  if c.name not  in ('name', 'geoid') and not c.name.endswith('_id')]
+            select2.append("trim(replace(name,'{name_remove}','')) as name2".format(name_remove=name_remove));
+
+
+            q = template.format(fields=','.join(s1_fields+s2_fields+ ['name1', 'name2']), 
+                                select1=','.join(select1),
+                                select2=','.join(select2),
+                                s1=s1, s2=s2,
+                                sumlev=sumlev, 
+                                joins=' AND '.join(joins))
+
+
+            print q
+
+            fields = fields + ['name']
+            
+            with self.database.inserter(table_name, replace=True) as ins:
+                for row in  self.database.connection.execute(q):
+                    
+                    row = list(row)
+
+                    name1 = row.pop()
+                    name2 = row.pop()
+                    
+                    if name2:
+                        name = name2
+                    elif name1:
+                        name = name1
+                    else:
+                        raise Exception("Missing Name")
+                    
+                    row.append(name)
+
+                    drow = dict(zip(fields, row))
+                    drow[table_name+'_id'] = None
+                    drow['geoid'] = None
+                 
+                    ins.insert(drow)
+        
+            self.database.detach(s1)
+            self.database.detach(s2)
+
+        return True
+
+    def load(self, year, config ):
+        from databundles.identity import PartitionIdentity
+        
+        year_suffix = str(year)[2:]
+        
+        ds = get_library().get(config.source) 
+        qt = config.template
+          
+        for qd in config.data:
+            table_name = str(qd[1])+year_suffix
+            
+            pid = PartitionIdentity(self.identity, grain=qd[1])
+            dp = self.partitions.find(pid) # Find puts id_ into partition.identity
+            
+            if not dp:
+                dp = self.partitions.new_partition(pid)
+                dp.database.create(copy_tables = False)
+                #dp.create_with_tables() 
+            
+            sp = ds.bundle.partitions.find(grain=qd[0])
+            print sp.identity.name, sp.database.path
+            
+            with dp.database.inserter(table_name) as ins:
+                q = qt.format(*qd)
+                print q
+                for row in  sp.database.session.execute(q):
+                    geo_id = 'foo'
+                    ins.insert((None, geo_id)+tuple(row))
+    
     def make_generated_geo(self):
         geo_file = self.filesystem.path(self.config.build.gengeoFile)
         with open(geo_file, 'w') as f:
@@ -123,41 +203,7 @@ class Bundle(BuildBundle):
                     
                 writer.writerow([table_name, str(k),None,type])
                         
-    def build(self):
-        
-        for year, cfg in  self.config.queries.items():
-           self.load(year, cfg)
-           
-        return True
 
-    def load(self, year, config ):
-        from databundles.identity import PartitionIdentity
-        
-        year_suffix = str(year)[2:]
-        
-        ds = get_library().get(config.source) 
-        qt = config.template
-          
-        for qd in config.data:
-            table_name = str(qd[1])+year_suffix
-            
-            pid = PartitionIdentity(self.identity, grain=qd[1])
-            dp = self.partitions.find(pid) # Find puts id_ into partition.identity
-            
-            if not dp:
-                dp = self.partitions.new_partition(pid)
-                dp.database.create(copy_tables = False)
-                #dp.create_with_tables() 
-            
-            sp = ds.bundle.partitions.find(grain=qd[0])
-            print sp.identity.name, sp.database.path
-            
-            with dp.database.inserter(table_name) as ins:
-                q = qt.format(*qd)
-                print q
-                for row in  sp.database.session.execute(q):
-                    geo_id = 'foo'
-                    ins.insert((None, geo_id)+tuple(row))
     
 import sys
 
